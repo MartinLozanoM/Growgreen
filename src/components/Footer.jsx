806da070390ab0b171cdf053d9347df0a0adc9c9
Luis-Ll <115672031+Luis-Ll@users.noreply.github.com>
2023-09-08 23:36:11 -0300

    change responsive mode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,19 @@ export const Footer = () => {
 return (
 <div className="footer p-4">
     <div className="row text-white p-5 text-center">
-        <div className="col">
+        <div className="col-md-3 col-sm-12">
             <h2 className="fw-bold">GROWGREEN</h2>
             <p>Con esta marca con forma abierta intentamos transmitir 
 un mensaje de transversalidad y oportunidad; y hacemos uso de tonos verdes para reforzar los valores de 
 triple impacto, naturaleza y tecnológicos del HUB</p>
         </div>
-        <div className="col">
+        <div className="col-md-3 col-sm-12">
             <h2>Politica</h2>
         </div>
-        <div className="col">
+        <div className="col-md-3 col-sm-12">
             <h2>Ayuda</h2>
         </div>
-        <div className="col">
+        <div className="col-md-3 col-sm-12">
             <h2>Nuestras redes</h2>
         </div>
     </div>
