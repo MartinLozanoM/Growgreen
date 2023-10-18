518af7e611337f68e4cd0425ad1e2463e7c7cbd3
Martin Lozano Muñoz <martinlozanomu@gmail.com>
2023-10-18 20:03:45 -0300

    agregue nuevos logos de las empresas aliadas, y acomode bien los tamaños para que se vean bien todos los logos

--- src/components/CompanySection.jsx
+++ src/components/CompanySection.jsx
@@ -6,21 +6,40 @@ import Row from "react-bootstrap/Row";
 import AgonLogo from "../assets/image/Agon.png";
 import ApiaLogo from "../assets/image/Apia.png";
 import Arqui3Logo from "../assets/image/Arqui3.png";
+import KosLogo from "../assets/image/kos-logo.jpeg";
+import SalomonLogo from "../assets/image/salomon.jpeg";
+import CeicytLogo from "../assets/image/ceicyt.jpeg";
 
 export const CompanySection = () => {
   return (
     <Container className="company-section">
       <h3 className="section-title text-center">Empresas Aliadas</h3>
       <hr />
-      <Row className="logo-row">
-        <Col xs={12} md={4}>
+      <Row className="logo-row row-gap-3">
+        <Col xs={12} md={4} className="companies-container">
           <Image src={AgonLogo} rounded fluid alt="Agon" />
         </Col>
-        <Col xs={12} md={4}>
-          <Image src={ApiaLogo} rounded fluid alt="Apia" />
+        <Col xs={12} md={4} className="companies-container">
+          <Image
+            width={250}
+            height={250}
+            src={CeicytLogo}
+            rounded
+            fluid
+            alt="Arqui3"
+          />
         </Col>
-        <Col xs={12} md={4}>
+        <Col xs={12} md={4} className="companies-container">
           <Image src={Arqui3Logo} rounded fluid alt="Arqui3" />
+        </Col>
+        <Col xs={12} md={4} className="companies-container">
+          <Image src={KosLogo} rounded fluid alt="Arqui3" />
+        </Col>
+        <Col xs={12} md={4} className="companies-container">
+          <Image src={SalomonLogo} rounded fluid alt="Arqui3" />
+        </Col>
+        <Col xs={12} md={4} className="companies-container">
+          <Image src={ApiaLogo} rounded fluid alt="Apia" />
         </Col>
       </Row>
     </Container>
